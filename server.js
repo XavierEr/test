1c4b9f3fa032d979b37e5ce7175e5977711b9793
Xavier Er <xavier_er@live.com.sg>
2018-09-25 17:38:12 +0800

    Enable cors

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
 
-var hotels = require('./routes/hotels');
+const hotels = require('./routes/hotels');
 
-var app = express();
+const app = express();
+
+app.use(cors())
 
 app.set('port', (process.env.PORT || 80));
 
 app.use('/api/hotels', hotels);
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
